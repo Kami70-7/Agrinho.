f5310281ed3ef33cd0696ac0d948d71f4048d034
Kamilly <cordeiro.santos.kamilly@escola.pr.gov.br>
2025-05-30 19:54:22 +0000

    Finalizando o Java

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const titulo = document. getElementById('titulo')
-let pos = 0;
+const h2s = document.querySelectorAll('h2');
 
-function mover () {
-    if (pos < 300){
-        pos++;
-        titulo.style.left = pos + 'px';
-        requestAnimationFrame(mover);// animação mais suave
-    }
-}
-  
-// Começa a animação ao carregar a página
+setInterval(() => {
+      h2s.forEach(h2 => {
+        h2.style.color = h2.style.color === 'rgb(78, 66, 42)' ? 'rgb(240, 0, 248)' : 'rgb(78, 66, 42)';
+      });
+    }, 500); 
+
+const h1s = document.querySelectorAll('h1');
+setInterval(() => {
+      h1s.forEach(h1 => {
+        h1.style.color = h1.style.color === 'rgb(194, 135, 9)' ? 'rgb(255, 255, 255)' : 'rgb(194, 135, 9)'
+      });
+    }, 500); 
+
 window.onload = mover;
