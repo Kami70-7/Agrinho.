221964028d83027e7ba1ecafd2fdd99f4eed48ea
Kamilly <cordeiro.santos.kamilly@escola.pr.gov.br>
2025-05-30 23:40:05 +0000

    Finalizando

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const h2s = document.querySelectorAll('h2');
 
 setInterval(() => {
       h2s.forEach(h2 => {
-        h2.style.color = h2.style.color === 'rgb(78, 66, 42)' ? 'rgb(240, 0, 248)' : 'rgb(78, 66, 42)';
+        h2.style.color = h2.style.color === 'rgb(114, 76, 0)' ? 'rgb(180, 15, 189)' : 'rgb(114, 76, 0)';
       });
     }, 500); 
 
 const h1s = document.querySelectorAll('h1');
 setInterval(() => {
       h1s.forEach(h1 => {
-        h1.style.color = h1.style.color === 'rgb(194, 135, 9)' ? 'rgb(255, 255, 255)' : 'rgb(194, 135, 9)'
+        h1.style.color = h1.style.color === 'rgb(194, 135, 9)' ? 'rgb(244, 255, 205)' : 'rgb(194, 135, 9)'
       });
     }, 500); 
 
